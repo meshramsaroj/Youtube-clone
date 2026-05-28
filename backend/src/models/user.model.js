3f3ef3767967ec59c96a4cdb30e6b9ec70baeb4e
sarojmeshram <saroj.g@morressier.com>
2026-05-28 09:36:37 +0530

    Impletemented password encryption and jwt token genration

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -47,5 +49,48 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// create password in encrypted form on save
+userSchema.pre("save", async function (next) {
+  if (!this.isModified(this.password)) return next(); // if password is not modified then don't call below lines of code
+  this.password = bcrypt.hash(this.password, 10); // we replace existing password data with encrypted password which is done by bcrypt.hash method. this method works like (variable name, encryption key/ or round)
+  next();
+});
+
+// check password whether it is correct or now by decrypting the same
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+//generate access token
+userSchema.methods.generateAccessToken = function () {
+  jwt.sign(
+    {
+      // data which needs to be send to token to access it on browser
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+// generate refresh token
+userSchema.methods.generateRefreshToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
